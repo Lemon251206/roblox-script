9844f0441ebaa87048a30c7999634c0e3b3af424
Lemon251206 <73783092+Lemon251206@users.noreply.github.com>
2022-08-12 12:52:29 +0700

    Update anime-adventures.lua

--- anime-adventures.lua
+++ anime-adventures.lua
@@ -221,9 +221,9 @@ end;
 function wait_wave()
     while(wait(0.1)) do
         if isFinished() then
-            wait(1);
+            wait(5);
             webhook('https://discord.com/api/webhooks/994515134380781668/dH6PDbmHltUeVnKJUVxoRJ2S2n0imhSMhO0ON1RGKtNrzycqdcRzo2OeeLKHuPRaoD79');
-            wait(4);
+            wait(1);
             back_to_lobby();
             break;
         end
